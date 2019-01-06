18f1c8158178e41dc4934c504af118ea7c2f6872
taneresme <tnr.esme@gmail.com>
2019-01-06 13:24:04 +0300

    some simple changes

--- app/src/main/java/com/example/mk0730/alkolmetre/utils/ReminderUtilities.java
+++ app/src/main/java/com/example/mk0730/alkolmetre/utils/ReminderUtilities.java
@@ -14,9 +14,7 @@ import com.firebase.jobdispatcher.Trigger;
 import java.util.concurrent.TimeUnit;
 
 public class ReminderUtilities {
-        private static final int REMINDER_INTERVAL_MINUTES = 1;
-        private static final int REMINDER_INTERVAL_SECONDS = (int) (TimeUnit.MINUTES.toSeconds(REMINDER_INTERVAL_MINUTES));
-        private static final int SYNC_FLEXTIME_SECONDS = REMINDER_INTERVAL_SECONDS;
+        private static final int REMINDER_INTERVAL_SECONDS = 60;
 
         private static final String REMINDER_JOB_TAG = "recommend_favorite_reminder_tag";
         private static boolean sInitialized;
@@ -28,14 +26,10 @@ public class ReminderUtilities {
                     /* The Service that will be used to write to preferences */
                     .setService(ReminderFirebaseJobService.class)
                     .setTag(REMINDER_JOB_TAG)
-                    .setLifetime(Lifetime.FOREVER)
                     .setRecurring(true)
-                    .setTrigger(Trigger.executionWindow(
-                            REMINDER_INTERVAL_SECONDS,
-                            REMINDER_INTERVAL_SECONDS + SYNC_FLEXTIME_SECONDS))
-                    .setReplaceCurrent(true)
+                    .setTrigger(Trigger.executionWindow(0, REMINDER_INTERVAL_SECONDS))
                     .build();
-            dispatcher.schedule(constraintReminderJob);
+            dispatcher.mustSchedule(constraintReminderJob);
             sInitialized = true;
         }
     }
